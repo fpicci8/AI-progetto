fd545639c3752c46aac0b38f658fc96acab834e2
iris119 <i.floris28@studenti.unica.it>
2026-02-22 12:01:54 +0100

    numeri aggiunti manualmente

--- sudoku_v2.py
+++ sudoku_v2.py
@@ -5,22 +5,27 @@ import matplotlib.pyplot as plt
 #creazione matrice 9x9 
 A=np.zeros((9, 9), dtype=int)
 num_elementi =70
-A[:,0]=[1,3,5,8,6,2,4,9,7]
-A[1:7,1]=[9,7,5,1,3,6]
-A[8,1]=8
-A[0:3,2]=[8,6,2,7]
-A[5:8,2]=[4,1,5,3]
-A[:,3]=[2,4,3,6,8,9,5,7,1]
-A[0,4]=7
-A[2:5,4]=[9,2,3,5]
-A[7,4]=6
-A[0:2,5]=[5,8,6]
-A[4:8,5]=[7,1,2,3,9]
-A[1:8,6]=[7,1,3,5,8,9,4,2]
-A[0:6,7]=[9,5,4,1,2,7,3]
-A[8,7]=6
-A[0:4,8]=[3,2,8,9,4]
-A[6:8,8]=[7,1,5]
+A[1,0]=5
+A[5:,0]=[3,1,7,9]
+A[0:2,1]=[6,8]
+A[3,1]=9
+A[5:7,1]=[5,3]
+A[8,1]=4
+A[1:3,2]=[1,3]
+A[6:8,2]=[5,8]
+A[0,3]=3
+A[2:5,3]=[5,8,7]
+A[7,3]=6
+A[4,4]=4
+A[7,4]=1
+A[0:2,5]=[8,9]
+A[3:6,5]=[2,5,6]
+A[7:,5]=[3,7]
+A[0:2,6]=[1,6]
+A[4:6,6]=[9,8]
+A[7:,6]=[4,3]
+A[4,7]=3
+A[4:6,8]=[6,4]
 #CREAZIONE DELLA FIGURA
 fig, ax = plt.subplots()
 
@@ -56,9 +61,7 @@ B = np.empty((9,9), dtype=object)
 for i in range(9):
     for j in range(9):
         if A[i, j] == 0:
-            elementi_riga = set(A[i, :]) - {0}
-            elementi_colonna = set(A[:, j]) - {0}
-            elementi_quadrante = set(A[i//3*3:(i//3+1)*3, j//3*3:(j//3+1)*3].flatten()) - {0} 
+            [elementi_riga, elementi_colonna, elementi_quadrante] = elementi(i, j)
             B[i, j] = set(range(1, 10)) - elementi_riga - elementi_colonna - elementi_quadrante
             # offset dentro la cella (1..9) in una griglia 3x3
             pos = {
@@ -74,4 +77,63 @@ for i in range(9):
         else:
             B[i, j] = A[i, j]
 plt.show()
+dim_min=10
 #fig.canvas.draw_idle()
+
+
+#funzione che restituisce gli elementi presenti nella riga, colonna e quadrante di una cella (i, j)
+def elementi(i, j):
+    global B
+    global A
+    elementi_riga = set(A[i, :]) - {0}
+    elementi_colonna = set(A[:, j]) - {0}
+    elementi_quadrante = set(A[i//3*3:(i//3+1)*3, j//3*3:(j//3+1)*3].flatten()) - {0} 
+    return elementi_riga, elementi_colonna, elementi_quadrante
+
+#minimum remaing value (MRV) heuristic
+def mrv():
+    global B
+    global A
+    for i in range(9):
+        for j in range(9):
+            if A[i, j] == 0:
+                if len(B[i, j]) < dim_min:
+                    dim_min = len(B[i, j])
+                    cella = (i, j)
+                    return cella
+                
+
+#degree heuristic           
+def degree():
+    global B
+    global A
+    dim_min=20
+    for i in range(9):
+        for j in range(9):
+            if A[i, j] == 0:
+                [elementi_riga, elementi_colonna, elementi_quadrante] = elementi(i, j)
+                grado = len(elementi_riga) + len(elementi_colonna) + len(elementi_quadrante)
+                if grado < dim_min:
+                    dim_min = grado
+                    cella = (i, j)
+                    return cella
+                
+#least constraining value (LCV) heuristic
+def lcv(i, j):
+    global B
+    global A
+    valori = []
+    for num in B[i, j]:
+        count = 0
+        for k in range(9):
+            if A[i, k] == 0 and num in B[i, k]:
+                count += 1
+            if A[k, j] == 0 and num in B[k, j]:
+                count += 1
+        for m in range(i//3*3, (i//3+1)*3):
+            for n in range(j//3*3, (j//3+1)*3):
+                if A[m, n] == 0 and num in B[m, n]:
+                    count += 1
+        valori.append((num, count))
+    valori.sort(key=lambda x: x[1])
+    return [val[0] for val in valori]
